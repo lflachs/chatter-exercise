638ee03b63843a46e54b8bf65ff9ef193878845e
Leonard <leonard.flachs@gmail.com>
2021-08-11 13:55:34 +0200

    exercise ready

--- src/common/components/UserProfile/UserProfile.js
+++ src/common/components/UserProfile/UserProfile.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import './_user-profile.scss';
+import React from "react";
+import "./_user-profile.scss";
 
 function getInitials(string) {
-  return string.match(/\b(\w)/g).slice(0, 2).join('').toUpperCase();
+  return string
+    .match(/\b(\w)/g)
+    .slice(0, 2)
+    .join("")
+    .toUpperCase();
 }
 
 export default function UserProfile({ color, name, icon }) {
